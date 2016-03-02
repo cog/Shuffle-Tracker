4cf19e4b70ea7064d45f9d45d6ded323269a3880
José Castro <cog@cpan.org>
2016-03-02 14:19:29 +0000

    new deck order is now a constant

--- shuffle_tracker/shuffle_tracker.py
+++ shuffle_tracker/shuffle_tracker.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Track cards through shuffles and cuts"""
 
-cards = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
+CARDS = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
          'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
          'KD', 'QD', 'JD', '10D', '9D', '8D', '7D', '6D', '5D', '4D', '3D', '2D', 'AD',
          'KS', 'QS', 'JS', '10S', '9S', '8S', '7S', '6S', '5S', '4S', '3S', '2S', 'AS']
@@ -9,7 +9,7 @@ cards = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH'
 class Deck(object):
     """decks of cards: shuffles and cuts"""
     def __init__(self):
-        self.deck = cards
+        self.deck = CARDS
 
     def riffle_shuffle(self):
         """riffle shuffles a deck"""
