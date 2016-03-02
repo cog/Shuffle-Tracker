82f0f93a139c0aa7fec8df77a3fce4583174be0d
José Castro <cog@cpan.org>
2016-03-02 14:11:32 +0000

    added missing docstrings

--- shuffle_tracker/shuffle_tracker.py
+++ shuffle_tracker/shuffle_tracker.py
@@ -6,26 +6,34 @@ cards = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH'
          'KS', 'QS', 'JS', '10S', '9S', '8S', '7S', '6S', '5S', '4S', '3S', '2S', 'AS']
 
 class Deck:
+    """decks of cards: shuffles and cuts"""
     def __init__(self):
         self.deck = cards
 
     def riffle_shuffle(self):
+        """riffle shuffles a deck"""
         return self.deck
 
     def faro(self):
+        """faro shuffles a deck"""
         return self.deck
 
     def cut(self):
+        """cuts a deck"""
         return self.deck
 
     def find(self):
+        """finds a card by its position or by its name"""
         return self.deck
 
     def distance(self):
+        """finds the distance between two cards"""
         return self.deck
 
     def find_card_before(self):
+        """finds the card immediately before another card"""
         return self.deck
 
     def find_card_after(self):
+        """finds the card immediately after another card"""
         return self.deck
